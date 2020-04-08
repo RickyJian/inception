e79ec4b63db867c494e00bb663d83fe49d920094
rjbc <rjricky03bc@gmail.com>
2020-04-08 22:14:18 +0800

    add header sticky

---
+++ html/js/nav.js
@@ -0,0 +1,14 @@
+window.onscroll = function () {
+  navSticky();
+};
+
+var navbar = document.getElementsByTagName("nav")[0];
+var sticky = navbar.offsetTop;
+
+function navSticky() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("fixed-top");
+  } else {
+    navbar.classList.remove("fixed-top");
+  }
+}
